c15f065350eaa9db83c7f73d957100ed09317591
KPPA <kalpnapatel275@gmail.com>
2018-09-24 10:15:08 -0400

    added express

--- app.js
+++ app.js
@@ -1,44 +1,40 @@
-let http = require('http');
+var express = require('express')
+var app = express()
+let req = require('request')
+
 let latitude = 39.963692
 let longitude = -75.139946
 let secretKey = 'b2de6e9cb127bd3babbe5c8696a790fb'
 let url = `https://api.darksky.net/forecast/${secretKey}/${latitude},${longitude}`
 
-    http.createServer((request, response) => {
-        let req = require('request')
-        req(url, (error, res, data) => {
-            
-            let body = JSON.parse(data)
-            response.writeHeader(200, {"Content-Type": "text/html"});
-            var utcSeconds = body.currently.time;
-            var localDate = new Date(0);
-            localDate.setUTCSeconds(utcSeconds);
-            response.end(`<!doctype html>
-            <html>
-                <head>
-                <title>Weather API</title>
-            
-                <meta charset="utf-8">
-                <meta http-equiv="Content-type" content="text/html; charset=utf-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1">   
-                </head>
-            
-                <body>
-                    <h1>Weather information for Philadelphia</h1>
-                    <ul>
-                        <li><strong>Recorded Time(timezone): </strong><span>${localDate}(${body.timezone})</span></li>
-                        <li><strong>Location Cordinates: </strong><span>${body.latitude}(lat.) and ${body.longitude}(long.)</span></li>
-                        <li><strong>Skies: </strong><span>${body.currently.summary}</span></li>
-                        <li><strong>Precipitation Probability: </strong><span>${body.currently.precipIntensity}</span></li>
-                        <li><strong>Precipitation Intensity: </strong><span>${body.currently.precipProbability}</span></li>
-                        <li><strong>Temperature: </strong><span>${body.currently.temperature}</span></li>
-                        <li><strong>Humidity: </strong><span>${body.currently.humidity}</span></li>
-                        <li><strong>Pressure: </strong><span>${body.currently.pressure}</span></li>
-                        <li><strong>Wind Speed: </strong><span>${body.currently.windspeed}</span></li>
-                        <li><strong>Visibility: </strong><span>${body.currently.visibility}</span></li>
-                    </ul>
-                </body>
-            </html>`)
-        })
-    }).listen(8091);
-    
+app.set('view engine', 'ejs')
+
+req(url, (error, res, data) => {
+  if (error) {
+    throw error
+  }
+  let body = JSON.parse(data)
+
+  var utcSeconds = body.currently.time
+  var localDate = new Date(0)
+  localDate.setUTCSeconds(utcSeconds)
+
+  app.get('*', function (req, res) {
+    res.render('../index.ejs', {
+      records: [
+        { label: 'Recorded Time(timezone)', value: `${localDate}(${body.timezone})` },
+        { label: 'Location Cordinates', value: `${body.latitude}(lat.) and ${body.longitude}(long.)` },
+        { label: 'Skies', value: `${body.currently.summary}` },
+        { label: 'Precipitation Probability', value: `${body.currently.precipIntensity}` },
+        { label: 'Precipitation Intensity', value: `${body.currently.precipProbability}` },
+        { label: 'Temperature', value: `${body.currently.temperature}` },
+        { label: 'Humidity', value: `${body.currently.humidity}` },
+        { label: 'Pressure', value: `${body.currently.pressure}` },
+        { label: 'Wind Speed', value: `${body.currently.windspeed}` },
+        { label: 'Visibility', value: `${body.currently.visibility}` }
+      ]
+    })
+  })
+})
+
+app.listen(8091)
